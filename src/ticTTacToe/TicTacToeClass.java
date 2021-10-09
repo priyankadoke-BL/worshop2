38fe0210fac53c1b3df97c48c30ecf38dbed7f12
Priyankadoke12345 <priyankaadoke@gmail.com>
2021-10-09 08:51:18 +0530

    workshop2

--- src/ticTTacToe/TicTacToeClass.java
+++ src/ticTTacToe/TicTacToeClass.java
@@ -4,5 +4,15 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class TicTacToeClass {
+    private char board[] = new char[10];
+
+    private char[] assign() {
+
+        for (int i = 1; i <= board.length - 1; i++) {
+            board[i] = ' ';
+        }
+        return board;
+    }
+
 
 }
